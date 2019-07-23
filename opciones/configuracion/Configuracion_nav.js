98ad82c98949604a3bb58f2cffa529c57f6c9e77
IgnacioEsparza <v.esparza02@ufromail.cl>
2019-07-23 16:29:01 -0400

    personalización botones de configuración

--- opciones/configuracion/Configuracion_nav.js
+++ opciones/configuracion/Configuracion_nav.js
@@ -1,12 +1,14 @@
-//This is an example code for NavigationDrawer//
 import React, { Component } from 'react';
-//import react in our code.
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
 export default class Configuracion_nav extends Component {
 
   render() {
+
+    var iconSize = 30;
+
     return (
       <View style={styles.MainContainer}>
         <View style={styles.BannerStyle}>
@@ -14,6 +16,50 @@ export default class Configuracion_nav extends Component {
         </View>
 
         <View style={styles.FragmentStyle}>
+
+          <View >
+            <TouchableOpacity>
+              <Text style={styles.textStyle}>Almacenamiento</Text>
+              <Icon name='ios-archive' size={iconSize} color='#000000' style={styles.iconStyle}></Icon>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.lineContainer}>
+            <View style={styles.lineaOptionStyle} />
+          </View>
+
+          <View >
+            <TouchableOpacity>
+              <Text style={styles.textStyle}>Notificaciones</Text>
+              <Icon name='ios-notifications' size={iconSize} color='#000000' style={styles.iconStyle}></Icon>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.lineContainer}>
+            <View style={styles.lineaOptionStyle} />
+          </View>
+
+          <View >
+            <TouchableOpacity>
+              <Text style={styles.textStyle}>Acerca de</Text>
+              <Icon name='ios-help-circle' size={iconSize} color='#000000' style={styles.iconStyle}></Icon>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.lineContainer}>
+            <View style={styles.lineaOptionStyle} />
+          </View>
+
+          <View >
+            <TouchableOpacity>
+              <Text style={styles.textStyle}>Ayuda</Text>
+              <Icon name='ios-alert' size={iconSize} color='#000000' style={styles.iconStyle}></Icon>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.lineContainer}>
+            <View style={styles.lineaOptionStyle} />
+          </View>
 
         </View>
       </View>
@@ -23,7 +69,8 @@ export default class Configuracion_nav extends Component {
 
 const styles = StyleSheet.create({
   MainContainer: {
-    flex: 1
+    flex: 1,
+    backgroundColor: '#FFFFFF',
   },
 
   BannerStyle: {
@@ -33,10 +80,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#648a64',
     alignItems: 'stretch',
     padding: 10,
+    marginBottom: 20
   },
 
   FragmentStyle: {
-    flex: 13,
+    flex: 12,
     backgroundColor: '#FFFFFF',
-  }
+    marginBottom: 20
+  },
+
+  optionsContainer: {
+    flex: 1
+  },
+
+  lineaOptionStyle: {
+    height: 0.9,
+    width: "90%",
+    backgroundColor: '#648a64'
+  },
+
+  lineContainer: {
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    marginBottom: 20
+  },
+  textStyle: {
+    fontSize: 20,
+    color: '#000',
+    paddingLeft: 50,
+  },
+
+  iconStyle: {
+    position: 'absolute',
+    left: 12,
+    top: -3
+  },
+
 });
